9f3d96b65ecf164941fa20e0d91d5c7dc7bc6aa7
Math- (Matt H.) <mthaney4@gmail.com>
2022-05-29 07:34:40 -0400

    Update preloader-script.js

--- wp-content/plugins/the-preloader/js/preloader-script.js
+++ wp-content/plugins/the-preloader/js/preloader-script.js
@@ -2,7 +2,7 @@ jQuery(window).load(function() {
 
     jQuery('#wptime-plugin-preloader').delay(400).fadeOut("slow");
 
-    setTimeout(wptime_plugin_remove_preloader, 2000);
+    setTimeout(wptime_plugin_remove_preloader, 3000);
 
     function wptime_plugin_remove_preloader() {
         jQuery('#wptime-plugin-preloader').remove();
