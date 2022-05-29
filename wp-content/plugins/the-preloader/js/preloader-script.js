31ebb7779bc6f504ac90894c3fcc109c72f56115
Math- (Matt H.) <mthaney4@gmail.com>
2022-05-29 07:34:18 -0400

    fdsg

--- wp-content/plugins/the-preloader/js/preloader-script.js
+++ wp-content/plugins/the-preloader/js/preloader-script.js
@@ -1,10 +1,11 @@
-jQuery(window).load(function() { 
+jQuery(window).load(function() {
 
-	jQuery('#wptime-plugin-preloader').delay(400).fadeOut("slow");
-	
-	setTimeout(wptime_plugin_remove_preloader, 4000);
-	function wptime_plugin_remove_preloader() {	
-		jQuery('#wptime-plugin-preloader').remove();
-	}
+    jQuery('#wptime-plugin-preloader').delay(400).fadeOut("slow");
+
+    setTimeout(wptime_plugin_remove_preloader, 2000);
+
+    function wptime_plugin_remove_preloader() {
+        jQuery('#wptime-plugin-preloader').remove();
+    }
 
 });
